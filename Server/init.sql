0903f5a82e945cc920931bba6faed11d52bac351
LittleDev <littledev0617@naver.com>
2023-08-11 21:56:54 +0900

    O 댓글 대댓 CRD 구현
    O 게시글 좋아요 구현
    
    O 행사/핫플 반경 거리 조회
    
    O 행사/핫플 카테고리별 조회

--- Server/init.sql
+++ Server/init.sql
@@ -7,24 +7,23 @@ CREATE TABLE `tb_user` (
 );
 
 CREATE TABLE `tb_place` (
-	`place_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`place_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`name`	varchar(40)	NOT NULL,
-	`description`	varchar(200)	NULL,
+	`category`	varchar(20)	NULL,
 	`state`	int	NULL	DEFAULT 0,
 	`start_date`	datetime	NOT NULL,
-	`end_date`	datetime	NOT NULL,
-	`latitude`	double	NOT NULL	DEFAULT 0,
-	`longitude`	double	NOT NULL	DEFAULT 0
+	`end_date`	datetime	NOT NULL
 );
 
 CREATE TABLE `tb_board` (
-	`board_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`board_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`place_id`	int	NOT NULL
 );
 
 CREATE TABLE `tb_comment` (
-	`comment_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`comment_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`post_id`	int	NOT NULL,
+	`user_id`	int	NOT NULL,
 	`is_reply`	tinyint	NOT NULL	DEFAULT 0	COMMENT '대댓',
 	`reply_id`	int	NULL	DEFAULT 0	COMMENT '대댓 참조 댓글',
 	`create_date`	datetime	NULL,
@@ -32,7 +31,7 @@ CREATE TABLE `tb_comment` (
 );
 
 CREATE TABLE `tb_post` (
-	`post_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`post_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`user_id`	int	NOT NULL,
 	`board_id`	int	NOT NULL,
 	`place_id`	int	NOT NULL,
@@ -44,24 +43,22 @@ CREATE TABLE `tb_post` (
 );
 
 CREATE TABLE `tb_feed` (
-	`feed_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`feed_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`place_id`	int	NOT NULL,
 	`title`	varchar(50)	NOT NULL,
 	`content`	varchar(1000)	NOT NULL
 );
 
 CREATE TABLE `tb_image` (
-	`image_id`	int PRIMARY KEY auto_increment	NOT NULL,
-	`booth_id`	int	NOT NULL	DEFAULT 0,
-	`feed_id`	int	NOT NULL	DEFAULT 0,
-	`order`	int	NULL	DEFAULT 0
+	`image_id`	int auto_increment PRIMARY KEY	NOT NULL,
+	`booth_id`	int,
+	`feed_id`	int,
+	`order`	int	DEFAULT 0
 );
 
 CREATE TABLE `tb_booth` (
-	`booth_id`	int PRIMARY KEY auto_increment	NOT NULL,
+	`booth_id`	int auto_increment PRIMARY KEY	NOT NULL,
 	`place_id`	int	NOT NULL,
-	`latitude`	double	NULL	DEFAULT 0,
-	`longitude`	double	NULL	DEFAULT 0,
 	`name`	varchar(20)	NULL,
 	`content`	varchar(100)	NULL,
 	`detail`	varchar(20)	NULL
@@ -77,39 +74,23 @@ CREATE TABLE `tb_organizer` (
 	`user_id`	int	NOT NULL
 );
 
-
-
-ALTER TABLE `tb_user` ADD CONSTRAINT `PK_TB_USER` PRIMARY KEY (
-	`user_id`
+CREATE TABLE `tb_likes` (
+	`post_id`	int	NOT NULL,
+	`user_id`	int	NOT NULL
 );
 
-ALTER TABLE `tb_place` ADD CONSTRAINT `PK_TB_PLACE` PRIMARY KEY (
-	`place_id`
+CREATE TABLE `tb_location` (
+	`booth_id`	int,
+	`place_id`	int,
+	`lat`	double,
+	`lon`	double
 );
 
-ALTER TABLE `tb_board` ADD CONSTRAINT `PK_TB_BOARD` PRIMARY KEY (
-	`board_id`,
-	`place_id`
-);
-
-ALTER TABLE `tb_comment` ADD CONSTRAINT `PK_TB_COMMENT` PRIMARY KEY (
-	`comment_id`
-);
-
-ALTER TABLE `tb_post` ADD CONSTRAINT `PK_TB_POST` PRIMARY KEY (
-	`post_id`
-);
-
-ALTER TABLE `tb_feed` ADD CONSTRAINT `PK_TB_FEED` PRIMARY KEY (
-	`feed_id`
-);
-
-ALTER TABLE `tb_image` ADD CONSTRAINT `PK_TB_IMAGE` PRIMARY KEY (
-	`image_id`
-);
-
-ALTER TABLE `tb_booth` ADD CONSTRAINT `PK_TB_BOOTH` PRIMARY KEY (
-	`booth_id`
+CREATE TABLE `tb_info` (
+	`info_id`	int auto_increment PRIMARY KEY	NOT NULL,
+	`place_id`	int	NOT NULL,
+	`title`	varchar(20)	NULL,
+	`content`	varchar(300)	NULL
 );
 
 ALTER TABLE `tb_board` ADD CONSTRAINT `FK_tb_place_TO_tb_board_1` FOREIGN KEY (
@@ -124,6 +105,13 @@ ALTER TABLE `tb_comment` ADD CONSTRAINT `FK_tb_post_TO_tb_comment_1` FOREIGN KEY
 )
 REFERENCES `tb_post` (
 	`post_id`
+);
+
+ALTER TABLE `tb_comment` ADD CONSTRAINT `FK_tb_user_TO_tb_comment_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `tb_user` (
+	`user_id`
 );
 
 ALTER TABLE `tb_post` ADD CONSTRAINT `FK_tb_user_TO_tb_post_1` FOREIGN KEY (
@@ -202,4 +190,41 @@ ALTER TABLE `tb_organizer` ADD CONSTRAINT `FK_tb_user_TO_tb_organizer_1` FOREIGN
 REFERENCES `tb_user` (
 	`user_id`
 );
+
+ALTER TABLE `tb_likes` ADD CONSTRAINT `FK_tb_post_TO_tb_likes_1` FOREIGN KEY (
+	`post_id`
+)
+REFERENCES `tb_post` (
+	`post_id`
+);
+
+ALTER TABLE `tb_likes` ADD CONSTRAINT `FK_tb_user_TO_tb_likes_1` FOREIGN KEY (
+	`user_id`
+)
+REFERENCES `tb_user` (
+	`user_id`
+);
+
+ALTER TABLE `tb_location` ADD CONSTRAINT `FK_tb_booth_TO_tb_location_1` FOREIGN KEY (
+	`booth_id`
+)
+REFERENCES `tb_booth` (
+	`booth_id`
+);
+
+ALTER TABLE `tb_location` ADD CONSTRAINT `FK_tb_place_TO_tb_location_1` FOREIGN KEY (
+	`place_id`
+)
+REFERENCES `tb_place` (
+	`place_id`
+);
+
+ALTER TABLE `tb_info` ADD CONSTRAINT `FK_tb_place_TO_tb_info_1` FOREIGN KEY (
+	`place_id`
+)
+REFERENCES `tb_place` (
+	`place_id`
+);
+
+
 INSERT INTO tb_user VALUES(0,0);
